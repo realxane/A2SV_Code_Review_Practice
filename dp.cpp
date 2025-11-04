3ba5c6c979d48fce98be003ed9055d3e87f1036f
realxane <sow72567555@gmail.com>
2025-11-04 18:53:51 +0000

    dp problem

--- dp.cpp
+++ dp.cpp
@@ -4,7 +4,6 @@
 #include <bits/stdc++.h>
  
 using namespace std;
-using ll = long long;
  
 #ifdef LOCAL
 #include "algo/debug.h"
@@ -20,18 +19,18 @@ int main() {
   while(t--){
     int n;
     cin>>n;
-    vector<ll> a(n), c(n);
-    ll s=0;
+    vector<long long> a(n), c(n);
+    long long s=0;
     for(int i=0; i<n; i++) cin>>a[i];
     for(int i=0; i<n; i++) {
         cin>>c[i];
         s+=c[i];
     }
-    vector<ll> dp(n+1, 0);
+    vector<long long> dp(n+1, 0);
     dp[0]=c[0];
-    ll maxdp=dp[0];
+    long long maxdp=dp[0];
     for(int i=1; i<n; i++){
-        ll best=0;
+        long long best=0;
         for(int j=0; j<i; j++){
             if (a[j]<=a[i]) best = max(best, dp[j]);
         }
